73ea40a53ae9a0db6484223035e1e61788b9a73a
Forrest-Li <jspzjnoms@gmail.com>
2021-03-03 13:40:17 +0800

    Auth settings

--- RoberTA-Demo/ViewController.swift
+++ RoberTA-Demo/ViewController.swift
@@ -20,6 +20,18 @@ class ViewController: UIViewController {
         var username = ""
         ref = Database.database(url: "https://roberta-5eb35-default-rtdb.firebaseio.com/").reference()
         
+        // Auth
+        var actionCodeSettings = ActionCodeSettings()
+        actionCodeSettings.url = URL(string: "https://example.appspot.com")
+        actionCodeSettings.handleCodeInApp = true
+        actionCodeSettings.setAndroidPackageName("com.firebase.example", installIfNotAvailable: false, minimumVersion: "12")
+
+        let provider = FUIEmailAuth(authUI: FUIAuth.defaultAuthUI()!,
+                                    signInMethod: FIREmailLinkAuthSignInMethod,
+                                    forceSameDevice: false,
+                                    allowNewEmailAccounts: true,
+                                    actionCodeSetting: actionCodeSettings)
+        
         let userID = Auth.auth().currentUser?.uid //"qEhesKr1knRV85cenFKnXxVchT43"
         ref.child("users").child(userID!).observeSingleEvent(of: .value, with: { (snapshot) in
           // Get user value
